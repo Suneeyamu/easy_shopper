621291209012804c66e73a577738860cbaad9666
Srivalli Chennamsetti <deepikachennamsetti@gmail.com>
2025-02-16 20:55:35 +0530

    Designed Sign-in & Sign-up screens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './features/auth/sign-in/sign-in.component';
+import { SignUpComponent } from './features/auth/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "sign-in",
+    pathMatch: "full"
+  },
+  {
+    path: "sign-in",
+    component: SignInComponent 
+  },
+  {
+    path: "sign-up",
+    component: SignUpComponent 
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
