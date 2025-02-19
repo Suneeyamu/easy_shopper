27d93917f7504c74c2207d88b9551c24c84cf07d
Srivalli Chennamsetti <deepikachennamsetti@gmail.com>
2025-02-19 18:14:38 +0530

    Dashboard, ProductList, ProductDetails Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './features/auth/sign-in/sign-in.component';
 import { SignUpComponent } from './features/auth/sign-up/sign-up.component';
+import { NavbarComponent } from './features/common/navbar/navbar.component';
+import { FooterComponent } from './features/common/footer/footer.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { ProductListComponent } from './features/product-list/product-list.component';
+import { ProductDetailsComponent } from './features/product-details/product-details.component';
 
 const routes: Routes = [
   {
@@ -18,7 +22,18 @@ const routes: Routes = [
     path: "sign-up",
     component: SignUpComponent 
   },
-  {path:'dashboard', component: DashboardComponent}
+  {
+    path: "dashboard",
+    component: DashboardComponent 
+  },
+  {
+    path: "productList",
+    component: ProductListComponent 
+  },
+  {
+    path: "productDetails",
+    component: ProductDetailsComponent 
+  },
 
 ];
 
